96893d083bc4552969b192f49c924505e8bd2f14
Zehua Wang <wangzeh@umich.edu>
2022-12-15 19:44:01 -0500

    Update eval cosine sim scores

--- eval_sim_scores.sh
+++ eval_sim_scores.sh
@@ -4,16 +4,16 @@
 # Updated: November 8, 2022
 
 # slurm options: --------------------------------------------------------------
-#SBATCH --job-name=bert_clean_data
+#SBATCH --job-name=eval_sim_scores
 #SBATCH --mail-user=wangzeh@umich.edu
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=256GB
-#SBATCH --time=8:00:00
+#SBATCH --mem-per-cpu=128GB
+#SBATCH --time=2:00:00
 #SBATCH --account=stats_dept1
-#SBATCH --partition=largemem
+#SBATCH --partition=standard
 #SBATCH --output=/home/%u/SI650/SI650F22_FinalProject/logs/%x-%j.log
 #SBATCH --error=/home/%u/SI650/SI650F22_FinalProject/logs/%x-%j-E.log
 
@@ -25,4 +25,4 @@ module load python/3.9.12
 cd /home/wangzeh/SI650/SI650F22_FinalProject/
 source .venv/bin/activate
 # pip install -r requirements.txt
-python clean_data.py
+python evaluation.py
